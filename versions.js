5b459e438ff10b007865271fe920945d199dadda
Documenter.jl <documenter@juliadocs.github.io>
2026-08-28 00:40:53 +0000

    build based on adfcaba

--- versions.js
+++ versions.js
@@ -1,7 +1,9 @@
 var DOC_VERSIONS = [
+  "stable",
+  "v0.2",
   "v0.2.0-rc2",
   "v0.2.0-rc1",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "v0.2.0-rc2";
-var DOCUMENTER_STABLE = "v0.2.0-rc2";
+var DOCUMENTER_NEWEST = "v0.2.0";
+var DOCUMENTER_STABLE = "stable";
